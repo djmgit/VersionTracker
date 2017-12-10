dcdc6038b6d8e59668663bfa5a5efa881d79bf6d
djmgit <djmdeveloper060796@gmail.com>
2017-12-10 21:32:48 +0530

    Added flask-cors

--- app/main.py
+++ app/main.py
@@ -5,11 +5,13 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_admin import Admin, BaseView, expose
 from flask_admin.contrib.sqla import ModelView
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user
+from flask_cors import CORS
 import json
 import re
 import os
 
 app = Flask(__name__)
+CORS(app)
 if os.environ.get('DATABASE_URL') is None:
     #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///versions.sqlite3'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/deep'
